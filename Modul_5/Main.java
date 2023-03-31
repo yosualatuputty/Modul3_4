13396000cfd1c60038e1a116ef09ac292716ee67
yosualatuputty <127925422+yosualatuputty@users.noreply.github.com>
2023-03-31 08:27:23 +0700

    Modul 5

--- Modul_5/Main.java
+++ Modul_5/Main.java
@@ -1,72 +1,74 @@
 import java.util.Scanner;
 
 public class Main {
-static Peternakan bebek = new Peternakan("Bebek","Telur Biasa", 15, "Telur", 2, 2000, "butir");
-static Peternakan unta = new Peternakan("Unta", "Susu Putih",3, "Susu", 5, 60000, "liter");
-static Peternakan domba = new Peternakan("Domba", "Bulu Hitam", 4, "Bulu", 4, 15000, "kg");
-static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) {
-        
-        boolean active = true;
-        
-        System.out.println("Menu:");
-        System.out.println("1. Menampilkan Informasi Peternakan\n2. Menampilkan Total Pemasukan Harian\n3. Menampilkan Harga Setelah PPN\n4. Exit");
-        System.out.print("Masukkan Menu Anda: ");
-        int pilihan = sc.nextInt();
+    static Peternakan bebek = new Peternakan("Bebek", "Telur Biasa", 15, "telur", 2, 2000, "butir");
+    static Peternakan unta = new Peternakan("Unta", "Susu Putih", 3, "susu", 5, 60000, "liter");
+    static Peternakan domba = new Peternakan("Domba", "Bulu Hitam", 4, "bulu", 4, 15000, "kg");
+    static Scanner sc = new Scanner(System.in);
+    final static double ppn = 0.1;
 
-        while(active){
+    public static void main(String[] args) {
+
+        boolean active = true;
+        while (active) {
+
+            System.out.println("Menu:");
+            System.out.println(
+                    "1. Menampilkan Informasi Peternakan\n2. Menampilkan Total Pemasukan Harian\n3. Menampilkan Harga Setelah PPN\n4. Exit");
+            System.out.print("\nMasukkan Menu Anda: ");
+            int pilihan = sc.nextInt();
+
             switch (pilihan) {
                 case 1:
-                cetakInfo(); 
+                    cetakInfo();
+                    System.out.println("=".repeat(50));
+                    break;
                 case 2:
-
+                    cetakPemasukkan();
+                    break;
                 case 3:
-   
-                case 4: 
-                active = false;
+                    cetakPPN();
+                    break;
+                case 4:
+                    active = false;
                     break;
 
                 default:
+                    System.out.println("=".repeat(50));
                     System.out.println("Pilihan tidak valid!");
+                    System.out.println("=".repeat(50));
                     break;
             }
         }
     }
 
-    static void cetakInfo(){
-        breakLine();
-        System.out.println(bebek.cetakInfo());
-        breakLine();
-        System.out.println(unta.cetakInfo());
-        breakLine();
-        System.out.println(domba.cetakInfo());
-        breakLine();
+    static void cetakInfo() {
+        Peternakan.cetakInfo();
     }
 
-    static void cetakPemasukkan(){
-        breakLine();
-        bebek.cetakPemasukkan();
-        breakLine2();
-        unta.cetakPemasukkan();
-        breakLine2();
-        domba.cetakPemasukkan();
-        breakLine2();
+    static void cetakPemasukkan() {
+        System.out.println("=".repeat(50));
+        Peternakan.cetakPemasukkan();
         hitungPemasukkan();
-        breakLine();
-
-    }
-
-    static void hitungPemasukkan(){
-        double total = bebek.hitungPemasukkan() + unta.hitungPemasukkan() + domba.hitungPemasukkan();
-        System.out.println("Total Pemasukkan: " + total);
-    }
-    static void breakLine(){
         System.out.println("=".repeat(50));
     }
 
-    static void breakLine2(){
-        System.out.println("-".repeat(50));
+    static void cetakPPN() {
+        System.out.println("=".repeat(50));
+        Peternakan.cetakPPN(ppn);
+        hitungPPN();
+        System.out.println("=".repeat(50));
+
     }
 
+    static void hitungPemasukkan() {
+        double total = bebek.hitungPemasukkan() + unta.hitungPemasukkan() + domba.hitungPemasukkan();
+        System.out.printf("Total Pemasukkan : %.0f\n", total);
+    }
+
+    static void hitungPPN() {
+        double total = bebek.hitungPPN(ppn) + unta.hitungPPN(ppn) + domba.hitungPPN(ppn);
+        System.out.printf("Total Pemasukkan : %.0f\n", total);
+    }
 
 }
--- Modul_5/Peternakan.java
+++ Modul_5/Peternakan.java
@@ -1,5 +1,6 @@
+import java.util.ArrayList;
+
 public class Peternakan {
-    final double ppn = 0.1;
     private String binatang = "Binatang";
     private String ras = "Ras";
     private double jml = 0;
@@ -7,8 +8,10 @@ public class Peternakan {
     private double harga = 0;
     private String namaHasil = "Nama Hasil";
     private String satuan = "Satuan";
+    public static ArrayList<Peternakan> list = new ArrayList<Peternakan>();
 
-    Peternakan(String binatang, String ras, double jml, String namaHasil, double hasil, double harga, String satuan){
+    Peternakan(String binatang, String ras, double jml, String namaHasil, double hasil, double harga, String satuan) {
+        list.add(this);
         this.binatang = binatang;
         this.ras = ras;
         this.jml = jml;
@@ -19,13 +22,46 @@ public class Peternakan {
 
     }
 
-    public double hitungPemasukkan(){
+    public String buatPPN(double ppn) {
+        return String.format("Jenis\t\t : %s | %s\nPemasukkan\t : %.0f", binatang, ras, hitungPPN(ppn));
+    }
+
+    public double hitungPPN(double ppn) {
+        return hitungPemasukkan() + (hitungPemasukkan() * ppn);
+    }
+
+    public static void cetakPPN(double ppn) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).buatPPN(ppn));
+            System.out.println("-".repeat(50));
+        }
+    }
+
+    public double hitungPemasukkan() {
         return hasil * harga * jml;
     }
-    public String cetakPemasukkan(){
-        return String.format("Jenis\t\t: %s | %s\nPemasukkan\t &d", binatang, ras, hitungPemasukkan());
+
+    public String buatPemasukkan() {
+        return String.format("Jenis\t\t : %s | %s\nPemasukkan\t : %.0f", binatang, ras, hitungPemasukkan());
     }
-    public String cetakInfo(){
-        return String.format("Jenis\t\t\t: %s\nRas\t\t\t: %s\nJumlah\t\t\t: %.0f ekor\n%s/Ekor\t\t: %.0f %s\nTotal %s/Hari\t: %.0f %s\nHarga %s/%s\t: %.0f", binatang, ras, jml, namaHasil, hasil, satuan, namaHasil, jml * hasil, satuan, namaHasil, satuan, harga);
+
+    public static void cetakPemasukkan() {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).buatPemasukkan());
+            System.out.println("-".repeat(50));
+        }
+    }
+
+    public String buatInfo() {
+        return String.format(
+                "Jenis\t\t\t: %s\nRas\t\t\t: %s\nJumlah\t\t\t: %.0f ekor\n%s/Ekor\t\t: %.0f %s\nTotal %s/Hari\t: %.0f %s\nHarga %s/%s\t: %.0f",
+                binatang, ras, jml, namaHasil, hasil, satuan, namaHasil, jml * hasil, satuan, namaHasil, satuan, harga);
+    }
+
+    public static void cetakInfo() {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("=".repeat(50));
+            System.out.println(list.get(i).buatInfo());
+        }
     }
 }
